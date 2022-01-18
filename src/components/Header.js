fbaeb47d4f6d28e43b103f3a23e2e23518dc3888
Rohald van Merode <rohald_89@live.nl>
2022-01-18 13:42:26 +0100

    Added functionality to the navigation in the Courses component and added the Routes to the different courses

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,10 @@ const Header = () => (
   <header>
     <span className="icn-logo"><i className="material-icons">code</i></span>
     <ul className="main-nav">
-      <li><NavLink exact to="/">Home</NavLink></li>
-      <li><NavLink to="/about">About</NavLink></li>
+      <li><NavLink exact to="/" activeStyle={{ background: 'tomato' }}>Home</NavLink></li>
+      <li><NavLink to="/about" 
+      // activeClassName="activeClassTest"
+      >About</NavLink></li>
       <li><NavLink to="/teachers">Teachers</NavLink></li>
       <li><NavLink to="/courses">Courses</NavLink></li>
     </ul>    
