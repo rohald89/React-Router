7275157958a5cd89b9629e4a282025b5d038c997
Rohald van Merode <rohald_89@live.nl>
2022-01-23 14:46:55 +0100

    added inline functions for the style and className on the NavLinks in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,8 +8,11 @@ const Header = () => (
       <li><NavLink end to="/">Home</NavLink></li>
       {/* end prop instead of exact */}
       {/* This is a simple renaming of a prop to better align with the common practices of other libraries in the React ecosystem. */}
-      <li><NavLink to="/about">About</NavLink></li>
-      <li><NavLink to="/teachers">Teachers</NavLink></li>
+      <li><NavLink to="/about" style={({ isActive }) => ({ background: isActive && 'tomato'})}>About</NavLink></li>
+      {/* the activeStyle prop has been removed, to apply inline styles you now pass a function to the style */}
+      <li><NavLink to="/teachers" className={({ isActive }) => "nav-link" + (isActive && " activated")}>Teachers</NavLink></li>
+      {/* the activeClassName prop has been removed. To add a specific class when an element is active you now use a function for the className */}
+      {/* In the example above instead of the `active` class being added there will now be an `activated` class when the route matches the link */}
       <li><NavLink to="/courses">Courses</NavLink></li>
     </ul>    
   </header>
