8e0bfa9a65fde73caf532eb2848fa82ac6062980
Rohald van Merode <rohald_89@live.nl>
2022-01-23 14:17:31 +0100

    converted Home to a functional component instead of class component, added routes to App.js for about, teachers and courses routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Header from './Header';
+import Home from './Home';
+import About from './About';
+import Teachers from './Teachers';
+import Courses from './Courses';
 
 const App = () => (
-  <div className="container">
-
-  </div>
+  <BrowserRouter>
+    <div className="container">
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />}/>
+        <Route exact path="about" element={<About />}/>
+        <Route exact path="teachers" element={<Teachers />}/>
+        <Route exact path="courses" element={<Courses />} />
+      </Routes>
+    </div>
+  </BrowserRouter>
 );
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Home extends Component {    
-  
-  render() {
-    return (
-      <div className="main-content home">
+const Home = () => {
+  return (
+    <div className="main-content home">
         <h2>Front End Course Directory</h2>
         <p>This fun directory is a project for the <em>React Router Basics</em> course on Treehouse.</p>
         <p>Learn front end web development and much more! This simple directory app offers a preview of our course library. Choose from many hours of content, from HTML to CSS to JavaScript. Learn to code and get the skills you need to launch a new career in front end web development.</p>
@@ -12,8 +10,7 @@ class Home extends Component {
         <hr />
         
       </div>
-    );
-  }
+  )
 }
 
 export default Home;
