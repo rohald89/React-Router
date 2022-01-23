0b33b1748822c8d84387088d2b465426bfcf4b56
Rohald van Merode <rohald_89@live.nl>
2022-01-23 15:07:41 +0100

    added nested routes for the course topics

--- src/components/App.js
+++ src/components/App.js
@@ -7,16 +7,25 @@ import About from './About';
 import Teachers from './Teachers';
 import Courses from './Courses';
 
+import HTML from './courses/HTML';
+import CSS from './courses/CSS';
+import JavaScript from './courses/JavaScript';
+
 const App = () => (
   <BrowserRouter>
     <div className="container">
       <Header />
       <Routes>
-        <Route exact path="/" element={<Home />}/>
-        <Route exact path="about" element={<About title="About" />}/>
+        <Route exact path="/" element={<Home />} />
+        <Route path="about" element={<About title="About" />} />
         {/* No longer need the render prop to pass in props to the component, you can just pass them right in there with using `element` prop */}
-        <Route exact path="teachers" element={<Teachers />}/>
-        <Route exact path="courses" element={<Courses />} />
+        <Route path="teachers" element={<Teachers />} />
+        <Route exact path="courses" element={<Courses />} >
+          {/* Nested routes for /courses/:topic */}
+          <Route path="html" element={<HTML />} />
+          <Route path="css" element={<CSS />} />
+          <Route path="javascript" element={<JavaScript />} />
+        </Route>
       </Routes>
     </div>
   </BrowserRouter>
--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import { NavLink, Outlet } from 'react-router-dom';
+
 
 const Courses = () => (
   <div className="main-content courses">
     <div className="course-header group">
       <h2>Courses</h2> 
       <ul className="course-nav">
-        <li><a href='/courses/html'>HTML</a></li>
-        <li><a href='/courses/css'>CSS</a></li>
-        <li><a href='/courses/javascript'>JavaScript</a></li>
+        <li><NavLink to='/courses/html'>HTML</NavLink></li>
+        <li><NavLink to='/courses/css'>CSS</NavLink></li>
+        <li><NavLink to='/courses/javascript'>JavaScript</NavLink></li>
       </ul>
     </div>
     
-    {/* Write routes here... */}
+    
+    <Outlet />
+    {/* Routes are now nested in their parent route, they are now all available in App.js */}
+    {/* This Outlet component lets React know where to render the child components */}
   </div>
 );
 
